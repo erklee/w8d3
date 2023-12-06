5690b27a2a53efcbc44b515030dd7b346a65e9bf
Jacob Tabor <jtabor214@gmail.com>
2023-12-06 17:11:29 -0500

    Completed up to #innerBubbleSortLoop

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdBubbleSort.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/absurdBubbleSort.js
@@ -13,11 +13,23 @@ function askIfGreaterThan(el1, el2, callback) {
                 callback(false)
             }
         })
-
-}
-console.log(askIfGreaterThan(1, 2, 10))
+    }
 
 
 function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
-    
+    if (i === ( arr.length - 1)){
+        outerBubbleSortLoop(madeAnySwaps)
+        return;
+    }
+
+    askIfGreaterThan(arr[i]. arr[i+1], function(isGreaterThan){
+        if (isGreaterThan){
+            const temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp; 
+
+            madeAnySwaps = true
+        }
+        innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop)
+    })
 }
