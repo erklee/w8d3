e7895a1936ea60befb0e899af38420107571ff90
Jacob Tabor <jtabor214@gmail.com>
2023-12-06 15:34:13 -0500

    Completed addnumbers.js

---
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/addnumbers.js
@@ -0,0 +1,21 @@
+const readline = require("readline"); 
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function addNumbers(sum, numsLeft, completionCallback){
+    if (numsLeft > 0) {
+        reader.question('Enter a number', function(input){
+            let inputNumber = parseInt(input)
+            sum += inputNumber
+            console.log("Sum:" + sum)
+
+            addNumbers(sum, numsLeft - 1, completionCallback)
+        });    
+    } else{
+        completionCallback(sum);
+    }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
